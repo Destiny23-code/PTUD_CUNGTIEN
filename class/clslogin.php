df1ee77b775a1e35bd9657ed36f5fb2b4aa7c332
Đặng Bình <136682593+Destiny23-code@users.noreply.github.com>
2025-10-29 16:46:51 +0700

    Revert " "Chức năng lập phiếu nhập kho thành phẩm""

--- class/clslogin.php
+++ class/clslogin.php
@@ -5,7 +5,7 @@
         public function mylogin ($user, $pass){
             $pass = md5($pass);
             $link = $this->connect();
-            $sql = "select tk.iduser, username, password, phanquyen, nv.tenNV, nv.maNV
+            $sql = "select tk.iduser, username, password, phanquyen, nv.tenNV
              from taikhoan tk
             join nhanvien nv on nv.iduser = tk.iduser
             where tk.username = '$user' and tk.password = '$pass' limit 1";
@@ -18,20 +18,13 @@
                 $password = $row['password'];
                 $tenNV = $row['tenNV'];
                 $phanquyen = $row['phanquyen'];
-                if (session_status() == PHP_SESSION_NONE) session_start();
+                session_start();
                 $_SESSION['login'] = true;
                 $_SESSION['id'] = $id;
                 $_SESSION['user'] = $username;
                 $_SESSION['pass'] = $password;
                 $_SESSION['hoTen'] = $tenNV;
                 $_SESSION['phanquyen'] = $phanquyen;
-                
-                // Lấy mã nhân viên từ bảng nhanvien
-                $sql_nv = "SELECT maNV FROM nhanvien WHERE iduser = '$id' LIMIT 1";
-                $kq_nv = $link->query($sql_nv);
-                if ($kq_nv && $row_nv = $kq_nv->fetch_assoc()) {
-                    $_SESSION['maNV'] = $row_nv['maNV'];
-                }
                 
                 return $phanquyen;
                 //header("Location: index.php?act=admin"); exit();
--- layout/giaodien/khotp.php
+++ layout/giaodien/khotp.php
@@ -5,13 +5,12 @@
 
     <a href="#" class="active"><i class="bi bi-house-door me-2"></i>Trang chủ</a>
 
-  <div class="nav-section">KHO THÀNH PHẨM</div>
-  <a href="<?php echo isset(
-$base_path) ? $base_path : ''; ?>/pages/nhapkho/lo_danh_sach.php"><i class="bi bi-box2-heart me-2"></i>Danh sách lô sản phẩm</a>
-  <a href="<?php echo isset($base_path) ? $base_path : ''; ?>/pages/nhapkho/bao_cao_chat_luong.php"><i class="bi bi-file-earmark-bar-graph me-2"></i>Báo cáo chất lượng</a>
-  <a href="<?php echo isset($base_path) ? $base_path : ''; ?>/pages/nhapkho/nhapkho.php"><i class="bi bi-arrow-down-right-square me-2"></i>Nhập kho thành phẩm</a>
-  <a href="<?php echo isset($base_path) ? $base_path : ''; ?>/pages/nhapkho/xuatkho.php"><i class="bi bi-arrow-up-right-square me-2"></i>Xuất kho thành phẩm</a>
-  <a href="<?php echo isset($base_path) ? $base_path : ''; ?>/pages/nhapkho/thongke_tonkho.php"><i class="bi bi-bar-chart me-2"></i>Thống kê tồn kho</a>
-  <a href="<?php echo isset($base_path) ? $base_path : ''; ?>/pages/nhapkho/canh_bao.php"><i class="bi bi-exclamation-circle me-2"></i>Cảnh báo</a>
+      <div class="nav-section">KHO THÀNH PHẨM</div>
+      <a href="#"><i class="bi bi-box2-heart me-2"></i>Danh sách lô sản phẩm</a>
+      <a href="#"><i class="bi bi-file-earmark-bar-graph me-2"></i>Báo cáo chất lượng</a>
+      <a href="#"><i class="bi bi-arrow-down-right-square me-2"></i>Nhập kho thành phẩm</a>
+      <a href="#"><i class="bi bi-arrow-up-right-square me-2"></i>Xuất kho thành phẩm</a>
+      <a href="#"><i class="bi bi-bar-chart me-2"></i>Thống kê tồn kho</a>
+      <a href="#"><i class="bi bi-exclamation-circle me-2"></i>Cảnh báo</a>
 
   </div>
