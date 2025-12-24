58f27f4d5327c20bfb50057c2f407d42af6199c1
Destiny23-code <binhdang4131@gmail.com>
2025-12-24 21:00:50 +0700

    Update final

--- pages/qc/lbccl.php
+++ pages/qc/lbccl.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-date_default_timezone_set('Asia/Ho_Chi_Minh')
+date_default_timezone_set('Asia/Ho_Chi_Minh');
 session_start();
 require_once("../../class/clslogin.php");
 $p = new login();
